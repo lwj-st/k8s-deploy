05deec70a13b553bf87eb36a3b44ab8dc1b6f919
lwj-st <mileroy21573@gmail.com>
2026-03-17 21:39:09 +0800

    fix calico ip

--- Script/15-Deploy-cni.sh
+++ Script/15-Deploy-cni.sh
@@ -14,6 +14,13 @@ calico="$(artifact_get_path_by_name "cni.manifest.calico")"
 log_info "部署 Calico CNI..."
 log_command "kubectl apply -f \"${calico}\""
 
+if [ -n "${CALICO_IP_AUTODETECTION_METHOD:-}" ]; then
+  log_info "为 calico-node 设置 IP_AUTODETECTION_METHOD=${CALICO_IP_AUTODETECTION_METHOD}"
+  log_command "kubectl -n kube-system set env daemonset/calico-node IP_AUTODETECTION_METHOD=\"${CALICO_IP_AUTODETECTION_METHOD}\""
+else
+  log_info "未设置 CALICO_IP_AUTODETECTION_METHOD，保留 Calico 默认 IP 探测方式"
+fi
+
 log_info "CNI 部署完成"
 
 
